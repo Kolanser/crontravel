96e9b4a26a7b9d1cbd8d9383f133b832e66f6a41
Николай Слесарев <kolansers@yandex.ru>
2023-06-18 22:45:39 +0300

    добавлена фильрация туров по ценам, рейтингу и категориям

--- crontravel/api/filters.py
+++ crontravel/api/filters.py
@@ -1,4 +1,4 @@
-from django_filters import FilterSet, ChoiceFilter, BooleanFilter
+from django_filters import FilterSet, ChoiceFilter, NumberFilter
 from excursions.models import Excursion, TYPE_CHOICES, TRANSPORT_CHOICES
 
 
@@ -6,8 +6,21 @@ class ExcursionFilter(FilterSet):
     """Фильтр для экскурсий."""
     type_excursion = ChoiceFilter(choices=TYPE_CHOICES)
     transport = ChoiceFilter(choices=TRANSPORT_CHOICES)
+    min_rating = NumberFilter(field_name='rating', lookup_expr='gte')
+    max_rating = NumberFilter(field_name='rating', lookup_expr='lte')
+    min_price = NumberFilter(field_name='price', lookup_expr='gte')
+    max_price = NumberFilter(field_name='price', lookup_expr='lte')
 
 
     class Meta:
         model = Excursion
-        fields = ('city', 'categories', 'type_excursion', 'transport', )
+        fields = (
+            'city',
+            'categories',
+            'type_excursion',
+            'transport',
+            'min_rating',
+            'max_rating',
+            'min_price',
+            'max_price',
+            )
